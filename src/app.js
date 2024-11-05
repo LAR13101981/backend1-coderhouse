5651c7cf037145bc12a343d4281f80cd22c3476b
LAR13101981 <revillardla@gmail.com>
2024-11-05 10:53:30 -0300

    testing git commit

--- src/app.js
+++ src/app.js
@@ -1,3 +1,12 @@
 import express from 'express';
 
 const app = express();
+
+const PORT = 8080;
+
+app.use(express.json());
+app.use;
+
+app.listen(PORT, () => {
+  console.log(`Server levantado en http://localhost:${PORT}`);
+});
